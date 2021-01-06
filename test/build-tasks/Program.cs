5f9d981b4ff5189b004cf43cdf85d53d9c02427a
Harry <harrypierson@outlook.com>
2021-01-05 16:38:21 -0800

    update test

--- test/build-tasks/Program.cs
+++ test/build-tasks/Program.cs
@@ -2,5 +2,8 @@ public class Program
 {
     public static void Main()
     {
+#pragma warning disable 219
+        DevHawk.Contracts.ApocToken contract = null;
+#pragma warning restore 219
     }
 }
