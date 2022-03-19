f4efb1d28d0ad3c65e737eab6045ffb0754c95e9
Harry <harrypierson@outlook.com>
2022-03-18 19:21:22 -0700

    proc.WaitForExit();

--- src/build-tasks/ProcessRunner.cs
+++ src/build-tasks/ProcessRunner.cs
@@ -38,6 +38,7 @@ namespace Neo.BuildTasks
 
             process.Exited += (_, _) => 
             { 
+                process.WaitForExit();
                 while (!process.HasExited)
                 {
                     Thread.Sleep(50);
