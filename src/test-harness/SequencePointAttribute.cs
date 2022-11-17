875ae9d813abdc613205a1f2ae5496f06e9a4815
Harry <harrypierson@outlook.com>
2022-11-16 16:09:29 -0800

    SequencePointAttribute

---
+++ src/test-harness/SequencePointAttribute.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace NeoTestHarness
+{
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
+    public class SequencePointAttribute : Attribute
+    {
+        public string FileName { get; } = string.Empty;
+        public uint Address { get; }
+        public uint StartLine { get; }
+        public uint StartColumn { get; }
+        public uint EndLine { get; }
+        public uint EndColumn { get; }
+
+        public SequencePointAttribute(string fileName, uint address, uint startLine, uint startColumn, uint endLine, uint endColumn)
+        {
+            FileName = fileName;
+            Address = address;
+            StartLine = startLine;
+            StartColumn = startColumn;
+            EndLine = endLine;
+            EndColumn = endColumn;
+        }
+    }
+}
